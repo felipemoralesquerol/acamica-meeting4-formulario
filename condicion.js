20d96718f08e39836ba528d571fb17451a1c877f
Felipe Morales <felipe.morales.querol@gmail.com>
2021-06-22 22:38:02 -0300

    Agregado de archivo de condicion

---
+++ condicion.js
@@ -0,0 +1,19 @@
+console.log("Hola");
+
+function preguntar() {
+    console.log("Estoy dentro de preguntar");
+    let valor1 = parseInt(document.getElementById("numero1").value);
+    let valor2 = parseInt(document.getElementById("numero2").value);
+
+    if (valor1 > valor2) {
+        console.log("Numero 1 es mayor")
+    } else {
+        console.log("numero 2 es mayor")
+    };
+
+
+
+
+
+
+}
